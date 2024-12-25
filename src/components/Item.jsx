786299589844dc864b7098b98c042290ca3fc7c5
Parrrthz <parthgondaliya025@gmail.com>
2024-12-25 23:21:39 +0530

    completed fronted

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeartBtn from './HeartBtn'
 import { MdOutlineBathtub, MdOutlineBed, MdOutlineGarage } from 'react-icons/md'
 import { Link } from 'react-router-dom'
+import { CgRuler } from 'react-icons/cg'
 
 
 const Item = ({property}) => {
@@ -21,6 +22,7 @@ const Item = ({property}) => {
             <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBed /> {property.facilities.bedrooms}</div>
             <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineBathtub /> {property.facilities.bathrooms}</div>
             <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /> {property.facilities.parkings}</div>
+            <div className='flexCenter gap-x-2 border-r border-slate-900/50 pr-4 font-[500]'><MdOutlineGarage /><CgRuler /></div> 400
         </div>
         <p className="pt-2 mb-4 line-clamp-2">{property.description}</p>
         <div className='flexBetween'>
--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Searchbar from '../components/Searchbar'
+import { PROPERTIES } from '../constant/data'
+import Item from '../components/Item'
 
 const Listing = () => {
   return (
-    <div>Listing</div>
+    <main className="max-padd-container my-[99px]">
+      <div  className="max-padd-container py-10 xl:py22 bg-primary rounded-3xl">
+        <div>
+          <Searchbar />
+          {/* container */}
+          <div className="grid gap-6 grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mt-10">
+          { PROPERTIES.map((property)=>(
+                    <Item key={property.title} property={property}/>
+            ))
+         }
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
